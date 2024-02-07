0f2aa251bd2e4c590d182f405038bdff6d3cd9e5
Yik Ki (Jackie) Ma <yikki.ma@anu.edu.au>
2024-02-07 11:15:37 +1100

    Made QU-fitting script more general

--- run_qufit.py
+++ run_qufit.py
@@ -2,20 +2,26 @@ import os
 
 
 
-## Type in the full path to <do_QUfit_1D_mnest.py>
-qu_fit_path = '/priv/avatar/ykma/my_script/RM-Tools/RMtools_1D/do_QUfit_1D_mnest.py'
+'''
+Running for source 1: NVSS J183847-040042
+'''
+os.system('qufit J183847-040042.dat -m 1 --sampler nestle --ncores 4 --nlive 128')
+os.system('qufit J183847-040042.dat -m 2 --sampler nestle --ncores 4 --nlive 128')
+#os.system('qufit J183847-040042.dat -m 11 --sampler nestle --ncores 4 --nlive 128') ## This one can take a very long time; skipping
 
+'''
+Running for source 2: NVSS J184432+064257
+'''
+os.system('qufit J184432+064257.dat -m 1 --sampler nestle --ncores 4 --nlive 128')
+os.system('qufit J184432+064257.dat -m 2 --sampler nestle --ncores 4 --nlive 128')
+#os.system('qufit J184432+064257.dat -m 11 --sampler nestle --ncores 4 --nlive 128') ## This one can take a very long time; skipping
 
-
-os.system('python3 '+qu_fit_path+' J183847-040042.dat -m 1 --sampler nestle --ncores 4 --nlive 128')
-os.system('python3 '+qu_fit_path+' J183847-040042.dat -m 2 --sampler nestle --ncores 4 --nlive 128')
-#os.system('python3 '+qu_fit_path+' J183847-040042.dat -m 11 --sampler nestle --ncores 4 --nlive 128') ## This one can take a very long time
-os.system('python3 '+qu_fit_path+' J184432+064257.dat -m 1 --sampler nestle --ncores 4 --nlive 128')
-os.system('python3 '+qu_fit_path+' J184432+064257.dat -m 2 --sampler nestle --ncores 4 --nlive 128')
-#os.system('python3 '+qu_fit_path+' J184432+064257.dat -m 11 --sampler nestle --ncores 4 --nlive 128') ## This one can take a very long time
-os.system('python3 '+qu_fit_path+' J190559+000721.dat -m 1 --sampler nestle --ncores 4 --nlive 128')
-os.system('python3 '+qu_fit_path+' J190559+000721.dat -m 2 --sampler nestle --ncores 4 --nlive 128')
-os.system('python3 '+qu_fit_path+' J190559+000721.dat -m 11 --sampler nestle --ncores 4 --nlive 128')
+'''
+Running for source 3: NVSS J190559+000721
+'''
+os.system('qufit J190559+000721.dat -m 1 --sampler nestle --ncores 4 --nlive 128')
+os.system('qufit J190559+000721.dat -m 2 --sampler nestle --ncores 4 --nlive 128')
+os.system('qufit J190559+000721.dat -m 11 --sampler nestle --ncores 4 --nlive 128')
 
 
 
